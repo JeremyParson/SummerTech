4f6444e66f7d07e4c92413bab3cf7110191803ae
Jeremy Parson <Jsbparson@gmail.com>
2018-07-02 09:08:36 -0400

    MY NAME JEFF

--- src/test.java
+++ src/test.java
@@ -3,11 +3,11 @@ public class test {
 
 	public static void main(String[] args) {
 		MyArrayList test = new MyArrayList();
-		test.add("Im will not be removed! 0");
-		test.add("Im will not be removed! 1");
-		test.add("Im will be removed! 2");
-		test.add("Im will not be removed! 3");
-		test.add("Im will not be removed! 4");
+		test.add("I will not be removed! 0");
+		test.add("I will not be removed! 1");
+		test.add("I will be removed! 2");
+		test.add("I will not be removed! 3");
+		test.add("I will not be removed! 4");
 	
 		for(int x = 0; x < test.size(); x++) {
 			System.out.println(test.get(x));

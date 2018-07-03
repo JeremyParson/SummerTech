0de81cc2e0d02b9a233b1084364df723bf25c65c
Jeremy Parson <Jsbparson@gmail.com>
2018-07-03 09:13:32 -0400

    added: add index function, and added remove index function

--- src/linkedList/MainProcess.java
+++ src/linkedList/MainProcess.java
@@ -4,28 +4,34 @@ public class MainProcess {
 
 	public static void main(String[] args) {
 		MyLinkedList<String> test = new MyLinkedList<String>();
-		test.add("Test0");
-		test.add("Test1");
-		test.add("Test2");
-		test.add("Test3");
-		
+		test.add("Test");
+		test.add("Test");
+		test.add("Test");
+		test.add("Test");
+
 		System.out.println(test.size());
-		
-		for(int x = 0; x < test.size(); x++) {
-			System.out.println(test.get(x));
+
+		for (int x = 0; x < test.size(); x++) {
+			System.out.println(test.get(x) + x);
 		}
-		
+
 		System.out.println("---------------------------");
-		System.out.println("---------------------------");
-		
+		System.out.println("--------Add Index----------");
+
 		test.add(2, "I am in the middle!");
-		
-		for(int x = 0; x < test.size(); x++) {
-			System.out.println(test.get(x));
+
+		for (int x = 0; x < test.size(); x++) {
+			System.out.println(test.get(x) + x);
 		}
-		
+
 		System.out.println("---------------------------");
-		System.out.println("---------------------------");
+		System.out.println("---------Remove------------");
+
+		test.remove(2);
+
+		for (int x = 0; x < test.size(); x++) {
+			System.out.println(test.get(x) + x);
+		}
 	}
 
 }
--- src/linkedList/MyLinkedList.java
+++ src/linkedList/MyLinkedList.java
@@ -10,48 +10,57 @@ public class MyLinkedList<E> {
 	public void add(E input) {
 
 		Node temp = head;
-		while(temp.hasNext()) {	
+		while (temp.hasNext()) {
 			temp = temp.getNextNode();
 		}
 		temp.setNextNode(new Node(input));
 	}
-	
+
 	public E get(int input) {
 		Node temp = head;
-		for(int x = 0; x <= input; x++) {
-			if(temp.hasNext()) {
+		for (int x = 0; x <= input; x++) {
+			if (temp.hasNext()) {
 				temp = temp.getNextNode();
 			}
 		}
-				
+
 		return (E) temp.getStorage();
 	}
-	
+
 	public int size() {
 		Node temp = head;
 		int size = 0;
-		while(temp.hasNext()) {
-			if(temp.hasNext()) {
+		while (temp.hasNext()) {
+			if (temp.hasNext()) {
 				temp = temp.getNextNode();
 				size++;
-			}else {
+			} else {
 				break;
 			}
 		}
 		return size;
 	}
-	
+
 	public void add(int index, E input) {
 		Node temp = head;
-		for(int x = 0; x != index - 1; x++) {
-			if(temp.hasNext()) {
+		for (int x = 0; x <= index - 1; x++) {
+			if (temp.hasNext()) {
 				temp = temp.getNextNode();
-			}else {
+			} else {
 				break;
 			}
 		}
+		Node tempNextNode = temp.getNextNode();
 		temp.setNextNode(new Node(input));
-		Node temp2 = temp.getNextNode();
-		temp2.setNextNode(temp2.getNextNode());
+		Node newNode = temp.getNextNode();
+		newNode.setNextNode(tempNextNode);
+	}
+
+	public void remove(int index) {
+		Node temp = head;
+		for (int x = 0; x < index; x++) {
+			temp = temp.getNextNode();
+		}
+		temp.setNextNode(temp.getNextNode().getNextNode());
 	}
 }

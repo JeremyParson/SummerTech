66db5d1b02b7a7714b06d7cfa4c9ebd0971a759c
Jeremy Parson <Jsbparson@gmail.com>
2018-07-06 10:01:48 -0400

    hash maps are hard

--- src/hashMaps/MainProcess.java
+++ src/hashMaps/MainProcess.java
@@ -5,8 +5,25 @@ public class MainProcess {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyHashMap test = new MyHashMap();
-		test.put("E559D1FB1B687811", 69);
-		System.out.println(test.get("E559D1FB1B687811"));
+		test.put("E559D1FB1B687811", 1);
+		test.put("gdasgdsgasdgO0WR", 2);
+		test.put("asfrasoe;a[ep[pewf", 3);
+		test.put("raghuioajewaiopghp", 4);
+		test.put("gftdsagdafnswioGE", 5);
+		test.put("ioetjniowetpuPOFEWQ", 6);
+		test.put("agadsgagasgasdgsd", 7);
+		test.put("weatwetweatwtaetwet", 8);
+		test.put("gtaerygraweyhraweasd", 9);
+		System.out.println(test.get("E559D1FB1B687811")); //Should return 1
+		System.out.println(test.get("gdasgdsgasdgO0WR"));
+		System.out.println(test.get("asfrasoe;a[ep[pewf"));
+		System.out.println(test.get("raghuioajewaiopghp"));
+		System.out.println(test.get("gftdsagdafnswioGE"));
+		System.out.println(test.get("ioetjniowetpuPOFEWQ"));
+		System.out.println(test.get("agadsgagasgasdgsd"));
+		System.out.println(test.get("weatwetweatwtaetwet"));
+		System.out.println(test.get("gtaerygraweyhraweasd"));
+		System.out.print(test.size());
 		
 	}
 
--- src/hashMaps/MyHashMap.java
+++ src/hashMaps/MyHashMap.java
@@ -1,27 +1,41 @@
 package hashMaps;
 
-import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class MyHashMap {
-
-	private int[] values;
-	private String[] keys;
+	
+	private int counter;
+	private LinkedList<Integer>[] values;
+	private LinkedList<String>[] keys;
 
 	public MyHashMap() {
-		values = new int[8];
-		keys = new String[8];
+		values = new LinkedList[8];
+		keys = new LinkedList[8];
+		counter = 0;
 	}
 
 	public void put(String key, int value) {
-		values[hash(key)] = value;
-		keys[hash(key)] = key;
+		int index = hash(key);
+		values[index] = new LinkedList<Integer>();
+		keys[index] = new LinkedList<String>();
+		values[index].add(value);
+		keys[index].add(key);
+		
+		
+		counter++;
+		if(counter > Math.sqrt(values.length)) {
+			resize();
+		}
 	}
 	
 	public int get(String key) {
-		return values[hash(key)];
+		int index = hash(key);
+		int valIndex = keys[index].indexOf(key);
+		return values[index].get(valIndex);
 	}
 
-	public int hash(String key) {
+	private int hash(String key) {
 		int y = 0;
 		for (int x = 0; x < key.length() - 1; x++) {
 			y = key.charAt(x);
@@ -29,6 +43,26 @@ public class MyHashMap {
 		}
 		return Math.abs(y % keys.length);
 		//return Math.abs(test(key, 0) % keys.length);
+	}
+	
+	private void resize() {
+		//Resizing should be the square of the previous array length
+		counter = 0;
+		int[] tempValues = Arrays.copyOf(values, values.length);
+		String[] tempKeys = Arrays.copyOf(keys, keys.length);
+		
+		values = new int[values.length * values.length];
+		keys = new String[keys.length * keys.length];
+		
+		for(int x = 0; x < tempKeys.length - 1; x++) {
+			if(tempKeys[x] != null) {
+				put(tempKeys[x],tempValues[x]);
+			}
+		}
+	}
+	
+	public int size() {
+		return values.length;
 	}
 
 }
